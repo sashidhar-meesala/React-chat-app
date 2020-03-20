b2708bf3adb254f0ee4e84b4f05e474293f8d4ba
sashidhar-meesala <sashidharklu@gmail.com>
2020-03-20 13:24:31 -0400

    index.js and router.js files added

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
+
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
