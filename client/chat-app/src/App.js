c5cca00ccd2f6c87ca1d0faec1326ff57457ce7f
sashidhar-meesala <sashidharklu@gmail.com>
2020-03-23 19:36:17 -0400

    different components created for chat screen

--- client/chat-app/src/App.js
+++ client/chat-app/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Chat from './components/Chat/Chat.js';
+import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join.js';
 
 const App = () => {
