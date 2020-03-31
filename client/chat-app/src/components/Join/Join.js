b39a48d311ec4b7878bc17673eb050c9f08b714f
sashidhar-meesala <sashidharklu@gmail.com>
2020-03-31 00:35:01 -0400

    added iusestate, link to chat .js

--- client/chat-app/src/components/Join/Join.js
+++ client/chat-app/src/components/Join/Join.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import './Join.css'
 export default function Join(){
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
     
    return (
       <div className="formnOuterContainer">
         <div className="formInnerContainer">
           <h1 className="heading"> Chat Rooms</h1>
           <div>
-            <input placeholder="Name" className="formInput" type="text"  />
+            <input placeholder="Name" className="formInput" type="text"  onChange={(event) => setName(event.target.value)}/>
           </div>
           <div>
-            <input placeholder="Room" className="formInput button-margin" type="text"  />
+            <input placeholder="Room" className="formInput button-margin" type="text" onChange={(event) => setRoom(event.target.value)} />
           </div>
+          <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}></Link>
             <button className="button button-margin">Enter Room</button>
         </div>
       </div>
